7fb63f026b068b8b7e9bb59232bef3a0e3d7b9f4
REPLACE_ME_WITH_YOUR_NAME <corey_sokol@icloud.com>
2019-11-01 16:45:10 -0500

    Updated database table

--- services/login_service.js
+++ services/login_service.js
@@ -3,13 +3,16 @@ const db = require('./dbconnection-service');
 db;
 
 function createAcount(req, res) {
-    let id = req.body.id
-    let lname = req.body.last
+    let id = ''
     let fname = req.body.first
+    let lname = req.body.last
+    let email = req.body.email
     let address = req.body.address
     let city = req.body.city
-    let sql = "INSERT INTO Persons VALUES (?, ?, ?, ?, ?)";
-    db.query(sql, [id, lname, fname, address, city], (err, result) => {
+    let state = req.body.state
+    let password = req.body.password
+    let sql = "INSERT INTO UserStore VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    db.query(sql, [id, fname, lname, email, address, city, state, password], (err, result) => {
         if(err) {
             res.send("User does not exist");
         } else {
